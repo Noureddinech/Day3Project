c3a7ad6a2cf8a3ce12af4c597d915b1d973e979f
Noureddinech <Noureddineleroi@gmail.com>
2021-05-22 10:51:00 -0400

    Hello world

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,5 +3,6 @@ import com.sun.xml.internal.xsom.XSUnionSimpleType;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("world");
     }
 }
